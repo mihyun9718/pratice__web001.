397925182beeced777a8235564f49f3e389e9a81
mihyun9718 <algy9718@naver.com>
2024-08-27 17:07:43 +0900

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,14 @@
+$(function () {
+    $('.to_top').on('click', function () {
+        $('html, body').animate({ scrollTop: 0 }, 1000)
+    })
+});
+
+$(function () {
+    $('#flnk').on('change', function () {
+        const lnk = $(this).val();
+        if (lnk) {
+            window.open(lnk)
+        }
+    })
+});
